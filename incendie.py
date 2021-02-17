2809965c66455057fe0c40151938445513e3b8de
David DAULASIM <david.daulasim@ens.uvsq.fr>
2021-02-17 15:15:45 +0100

    debut

--- incendie.py
+++ incendie.py
@@ -8,4 +8,29 @@
 # Ramata Dia 
 # https://github.com/uvsq22000463/projet_incendie
 #########################################
-test
+
+import tkinter as tk
+
+#constante
+couleur_fond= "white"
+LARGEUR = 600
+HAUTEUR = 400
+
+#fonction
+
+
+
+
+#programme principale
+racine = tk.Tk()
+racine.title("incendie")
+
+#creation de widget
+canvas = tk.Canvas(racine, bg = couleur_fond, width = LARGEUR, height = HAUTEUR)
+
+#placement de widget
+canvas.grid()
+
+#boucle principale
+racine.mainloop
+
